fbcc280e7db39bea877202512cfa9d6f5b92259e
pattenbranden <patten.branden@gmail.com>
2020-01-28 19:31:12 -0500

    added firebase and jQuery calls

---
+++ assets/javascript/logic.js
@@ -0,0 +1,38 @@
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCwJ4l3bj7gwKZV9HesoNMQHX_ES4mWie8",
+    authDomain: "service-storage-dc859.firebaseapp.com",
+    databaseURL: "https://service-storage-dc859.firebaseio.com",
+    projectId: "service-storage-dc859",
+    storageBucket: "service-storage-dc859.appspot.com",
+    messagingSenderId: "130590530959",
+    appId: "1:130590530959:web:c882dee8bfa3fa02387ffa"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+var name = $("#nameInput").val().trim();
+var role = $("#roleInput").val().trim();
+var startDate = $("#startDateInput").val().trim();
+var monthsWorked;
+var monthlyRate = $("#ropInput").val().trim();
+var totalBilled;
+//   function takes in name role start date and monthly rate 
+// adds to firebase 
+// function to push data to list 
+// function to calculate the months worked
+//  and total billed 
+$("#submit").on("click", function (event) {
+    event.preventDefault();
+    var name = $("#nameInput").val().trim();
+    var role = $("#roleInput").val().trim();
+    var startDate = $("#startDateInput").val().trim();
+    var monthlyRate = $("#ropInput").val().trim();
+    database.ref().push({
+        name: name,
+        role: role,
+        startDate: startDate,
+        monthlyRate: monthlyRate
+      });
+}
+
